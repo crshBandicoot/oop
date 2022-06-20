70eda3c05e83076c00b972a9fdecac854ecf255c
Bogdan Pizhuk <bogdanpizuk2604@gmail.com>
2022-06-20 20:40:52 +0300

    format

--- BezierTest.py
+++ BezierTest.py
@@ -6,12 +6,10 @@ from Bezier import CubicBezierCurve
 class BezierTest(unittest.TestCase):
     def test_LinearInterpolation(self):
         bezier = CubicBezierCurve()
-        x0, y0, x1, y1, t = randrange(100), randrange(
-            100), randrange(100), randrange(100), randrange(0, 101)
+        x0, y0, x1, y1, t = randrange(100), randrange(100), randrange(100), randrange(100), randrange(0, 101)
         param = t/100
         expected = ((x1-x0)*param, (y1-y0)*param)
-        actual = (bezier.linearInterpolation((x0, y0), (x1, y1), param)
-                  [0]-x0, bezier.linearInterpolation((x0, y0), (x1, y1), param)[1]-y0)
+        actual = (bezier.linearInterpolation((x0, y0), (x1, y1), param)[0]-x0, bezier.linearInterpolation((x0, y0), (x1, y1), param)[1]-y0)
         for x, y in zip(expected, actual):
             with self.subTest(x=x, y=y):
                 self.assertAlmostEqual(x, y, 3)
@@ -23,12 +21,16 @@ class BezierTest(unittest.TestCase):
 
     def test_GetPoint(self):
         bezier = CubicBezierCurve()
-        p0, p1, t = (randrange(100), randrange(100)), (randrange(
-            100), randrange(100)), randrange(0, 101)
+        x0, y0, x1, y1, t = randrange(100), randrange(100), randrange(100), randrange(100), randrange(0, 101)
+        p0, p1 = (x0, y0), (x1, y1)
         param = t/100
-        expected = bezier.linearInterpolation(p0, p1, t)
-        actual = bezier.getPoint(p0, p0, p1, p1, t)
-        self.assertEqual(expected, actual)
+        vector = (x1-x0, y1-y0)
+        point = ((x0 + vector[0]*param), (y0 + vector[1]*param))
+        expected = point
+        actual = bezier.getPoint(p0, p0, p1, p1, param)
+        for x, y in zip(expected, actual):
+            with self.subTest(x=x, y=y):
+                self.assertAlmostEqual(x, y, delta = 10)
 
 
 if __name__ == '__main__':
